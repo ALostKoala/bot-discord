aa1333b7712973a6142b2a96f636444333275425
ALostKoala <32513663+ALostKoala@users.noreply.github.com>
2017-11-28 16:38:37 +0100

    version du 28/11/2017

---
+++ test bot TS/tts.ps1
@@ -0,0 +1,15 @@
+﻿$textFilePath = "D:\test bot TS\tmp_tts\file.txt"
+#important - make sure this directory path ends with a slash
+$outputDirectoryPath = "D:\test bot TS\tmp_tts\"
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$lineCounter = 0
+
+foreach($line in get-content $textFilePath) {
+	$speak.SetOutputToWaveFile($outputDirectoryPath + ($lineCounter -as [string]) + ".mp3")
+	echo ("Writing file " + $outputDirectoryPath + ($lineCounter -as [string]) + ".mp3")
+	$speak.Speak($line)
+	$lineCounter++
+}
+
+$speak.Dispose()
